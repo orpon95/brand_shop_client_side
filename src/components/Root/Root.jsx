7453d3a8dfe56d626f6aaba575945e8dab352f86
orpon95 <mdorpon95@gmail.com>
2023-10-19 00:29:34 +0600

    basic setup with react router dom

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
